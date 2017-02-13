1515e687015941cdb8516af438bbce7d5c120f3a
willi10 <willi10@r116-04.schule.willi-graf.local>
2017-02-13 15:20:47 +0100

    zwei Zeilen ergänzt

--- GitApp1/src/gitapp1/GitApp1.java
+++ GitApp1/src/gitapp1/GitApp1.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  * meine erste Änderung für Git
+ * meine zweite Änderung für Git
  */
 package gitapp1;
 
